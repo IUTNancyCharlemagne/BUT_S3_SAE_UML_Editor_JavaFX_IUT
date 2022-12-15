544cdd3e4ccfafc0c355e7fd24aaa1f0ad44b8da
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-12-15 11:18:52 +0100

    Model skeleton + interface Sujet

---
+++ src/model/Modele.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.List;
+
+public class Modele implements Sujet
+{
+
+    private List<Observateur> observateurs;
+    private List<Classe> classes;
+
+    public Modele()
+    {
+
+    }
+
+    public void ajouterFichier()
+    {
+
+    }
+
+    @Override
+    public void enregistrerObservateur(Observateur obs) {
+
+    }
+
+    @Override
+    public void supprimerObservateur(Observateur obs) {
+
+    }
+
+    @Override
+    public void notifierObservateurs() {
+
+    }
+}
