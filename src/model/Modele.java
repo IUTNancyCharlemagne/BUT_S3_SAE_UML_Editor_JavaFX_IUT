25b50d4a402b091cd509792074178ce1bcee5293
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-12-15 11:42:37 +0100

    Modele

--- src/model/Modele.java
+++ src/model/Modele.java
@@ -1,21 +1,40 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Modele implements Sujet
 {
-
     private List<Observateur> observateurs;
     private List<Classe> classes;
+    private FileComposite dossier;
+    private Format format;
 
     public Modele()
+    {
+        this.observateurs = new ArrayList<Observateur>();
+        this.classes = new ArrayList<Classe>();
+    }
+
+    public void ajouterFichiers()
     {
 
     }
 
-    public void ajouterFichier()
+    public void deplacerFichier(int dx, int dy)
     {
 
+    }
+
+    public List<FileComposite> getFichiers()
+    {
+        return this.dossier.list();
+    }
+
+
+
+    public void supprimerFichiers(FileComposite fichier)
+    {
     }
 
     @Override
