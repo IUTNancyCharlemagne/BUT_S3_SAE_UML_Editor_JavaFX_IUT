9c508ce1a77b64a87f3be67cfebc5aa416e38c4d
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-01-04 10:43:03 +0100

    implementation

---
+++ src/com/modele/elements/Implementation.java
@@ -0,0 +1,27 @@
+package com.modele.elements;
+
+public class Implementation
+{
+    private ClasseInterface initClasse, destinationClasse;
+
+    public Implementation(ClasseInterface initClasse, ClasseInterface destinationClasse) {
+        this.initClasse = initClasse;
+        this.destinationClasse = destinationClasse;
+    }
+
+    public ClasseInterface getInitClasse() {
+        return initClasse;
+    }
+
+    public void setInitClasse(ClasseInterface initClasse) {
+        this.initClasse = initClasse;
+    }
+
+    public ClasseInterface getDestinationClasse() {
+        return destinationClasse;
+    }
+
+    public void setDestinationClasse(ClasseInterface destinationClasse) {
+        this.destinationClasse = destinationClasse;
+    }
+}
