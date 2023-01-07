e7df2285283b6cd4ba4d3a8b940430f4837f85b7
Nicolas Kuenemann <nicolas.kuenemann3@etu.univ-lorraine.fr>
2023-01-08 00:01:06 +0100

    Division code

--- src/com/vue/VueDiagramme.java
+++ src/com/vue/VueDiagramme.java
@@ -17,70 +17,20 @@ import java.util.Random;
 
 public class VueDiagramme extends Pane implements Observateur {
 
-    private ArrayList<VueClasse> classesList;
     public void actualiser(Sujet sujet) {
-        //this.setVbarPolicy(ScrollBarPolicy.ALWAYS);
-        //this.setHbarPolicy(ScrollBarPolicy.ALWAYS);
-        this.getChildren().clear();
-        classesList = new ArrayList<>();
-        List<ClasseInterface> classes = sujet.getClasses();
         ScrollPane scrollPane = new ScrollPane();
+        this.getChildren().clear();
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
-        FlowPane pane = new FlowPane();
-        pane.setPrefSize(800,600);
-        for (ClasseInterface classe : classes) {
-            FabriqueDeVue fabriqueDeVue = new FabriqueVueClasse();
-            Group classeVue = new Group();
-            VueClasse vueElement = (VueClasse) fabriqueDeVue.creerVueElement();
-
-            vueElement.setPrefSize(200, 200);
-            vueElement.setTitle(classe.getNom());
-            vueElement.ajouterSeparateur();
-
-            for (Attribut attribut : classe.getAttributs()) {
-                fabriqueDeVue = new FabriqueVueAttribut();
-
-                VueElementClasse vueAttribut = (VueElementClasse) fabriqueDeVue.creerVueElement();
-
-                vueAttribut.setAccessibility(attribut.getVisibilite());
-                vueAttribut.setType(attribut.getType());
-                vueAttribut.setName(attribut.getNom());
-
-                vueElement.setAttribut(vueAttribut);
-            }
-            vueElement.ajouterSeparateur();
-            for (Methode methode : classe.getMethodes()) {
-                fabriqueDeVue = new FabriqueVueMethode();
-
-                VueElementClasse vueMethode = (VueElementClasse) fabriqueDeVue.creerVueElement();
-
-                vueMethode.setAccessibility(methode.getVisibilite());
-                vueMethode.setType(methode.getType());
-                vueMethode.setName(methode.getNom());
-                vueMethode.setParameters(methode.getParametres());
-
-                vueElement.setMethode(vueMethode);
-            }
-
-            for (Association association : classe.getAssociations()) {
-
-            }
-
-            for (Implementation implementation : classe.getImplementations()) {
-
-            }
-            classeVue.getChildren().add(vueElement);
-            classesList.add(vueElement);
-            pane.getChildren().add(classeVue);
-        }
-        scrollPane.setContent(pane);
+        VueFrabriqueClasses vueFrabriqueClasses = new VueFrabriqueClasses();
+        vueFrabriqueClasses.generer(sujet);
+        scrollPane.setContent(vueFrabriqueClasses);
         this.getChildren().add(scrollPane);
     }
 
 
     public VueDiagramme() {
         super();
-        this.setStyle("-fx-background-color: #FFFFFF");
+        this.setStyle("-fx-background-color: #ffffff");
     }
 }
