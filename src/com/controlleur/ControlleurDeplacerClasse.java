7e00bfa7b8f00ab376c07c390da5043c6e824e82
raphtrooper <raphmangin88@gmail.com>
2023-01-12 09:17:56 +0100

    Tansformation des TreeView<String> en TreeView<Text>

---
+++ src/com/controlleur/ControlleurDeplacerClasse.java
@@ -0,0 +1,26 @@
+package com.controlleur;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.*;
+import javafx.scene.layout.FlowPane;
+
+public class ControlleurDeplacerClasse implements EventHandler<MouseEvent> {
+
+    private double mouseX;
+    private double mouseY;
+    @Override
+    public void handle(MouseEvent mouseEvent) {
+        FlowPane p = (FlowPane) mouseEvent.getSource();
+        if (mouseEvent.getEventType() == MouseEvent.MOUSE_PRESSED){
+            mouseX = mouseEvent.getSceneX();
+            mouseY = mouseEvent.getSceneY();
+            p.toFront();
+        } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_DRAGGED){
+            double deltaX = mouseEvent.getSceneX() - mouseX;
+            double deltaY = mouseEvent.getSceneY() - mouseY;
+            p.relocate(p.getLayoutX() + deltaX, p.getLayoutY() + deltaY);
+            mouseX = mouseEvent.getSceneX();
+            mouseY = mouseEvent.getSceneY();
+        }
+    }
+}
--- src/com/controlleur/ControlleurGlisserDeposer.java
+++ src/com/controlleur/ControlleurGlisserDeposer.java
@@ -1,26 +1,11 @@
 package com.controlleur;
 
 import javafx.event.EventHandler;
-import javafx.scene.input.*;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.input.DragEvent;
 
-public class ControlleurGlisserDeposer implements EventHandler<MouseEvent> {
-
-    private double mouseX;
-    private double mouseY;
+public class ControlleurGlisserDeposer implements EventHandler<DragEvent> {
     @Override
-    public void handle(MouseEvent mouseEvent) {
-        FlowPane p = (FlowPane) mouseEvent.getSource();
-        if (mouseEvent.getEventType() == MouseEvent.MOUSE_PRESSED){
-            mouseX = mouseEvent.getSceneX();
-            mouseY = mouseEvent.getSceneY();
-            p.toFront();
-        } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_DRAGGED){
-            double deltaX = mouseEvent.getSceneX() - mouseX;
-            double deltaY = mouseEvent.getSceneY() - mouseY;
-            p.relocate(p.getLayoutX() + deltaX, p.getLayoutY() + deltaY);
-            mouseX = mouseEvent.getSceneX();
-            mouseY = mouseEvent.getSceneY();
-        }
+    public void handle(DragEvent dragEvent) {
+
     }
 }
